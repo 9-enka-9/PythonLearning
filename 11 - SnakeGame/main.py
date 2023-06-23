cfb450a171c4458d282d249b6aa928ea636367e2
En <minhen520tp@gmail.com>
2023-06-23 16:35:41 +0700

    done day 20 - Snake game (create snake, control and move snake, using OOP), learn methods: tracer, update in Turtle

--- 11 - SnakeGame/main.py
+++ 11 - SnakeGame/main.py
@@ -6,14 +6,19 @@ scr=t.Screen()
 scr.setup(width=600,height=600)
 scr.bgcolor("black")
 scr.title("Snake Game")
-scr.tracer(0)
-
+scr.tracer(0) # to make squares move without space
+scr.listen()
 snake=Snake()
+
+scr.onkey(fun=snake.up, key="Up")
+scr.onkey(fun=snake.down, key="Down")
+scr.onkey(fun=snake.left, key="Left")
+scr.onkey(fun=snake.right, key="Right")
 
 isGameOn=True
 while isGameOn:
-    scr.update()
-    time.sleep(0.2)
+    scr.update() # show the move of squares, use with tracer method above
+    time.sleep(0.1)
     snake.move()
     
     
--- 11 - SnakeGame/snake.py
+++ 11 - SnakeGame/snake.py
@@ -1,5 +1,10 @@
 import turtle as t
 
+MOVE_DISTANCE=20
+RIGHT=0
+UP=90
+LEFT=180
+DOWN=270
 
 class Snake():
     snake=[]
@@ -14,6 +19,7 @@ class Snake():
         newSquare.color("white")
         newSquare.pensize(width=20)
         newSquare.penup()
+        newSquare.speed(120)
         if len(self.snake)==0:
             newSquare.goto(0,0)
         else:
@@ -23,4 +29,25 @@ class Snake():
     def move(self):
         for segNumb in range(len(self.snake)-1, 0, -1):
             self.snake[segNumb].goto(self.snake[segNumb-1].pos())
-        self.snake[0].forward(20)
+        self.snake[0].forward(MOVE_DISTANCE)
+        
+    def up(self):
+        if self.snake[0].heading()==DOWN:
+            return
+        self.snake[0].setheading(90)
+    
+    def down(self):
+        if self.snake[0].heading()==UP:
+            return
+        self.snake[0].setheading(270)
+    
+    def left(self):
+        if self.snake[0].heading()==RIGHT:
+            return
+        self.snake[0].setheading(180)
+        
+    def right(self):
+        if self.snake[0].heading()==LEFT:
+            return 
+        self.snake[0].setheading(0)
+        
