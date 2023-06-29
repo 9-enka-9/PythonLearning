415a995a61c6c13b8de5fdf4d1f3a791f47c35c3
En <minhen520tp@gmail.com>
2023-06-29 17:34:55 +0700

    Done 2 paddles

--- 12 - PongGame/main.py
+++ 12 - PongGame/main.py
@@ -1,12 +1,25 @@
 import turtle as t
+from paddle import Paddle
 
 scr=t.Screen()
 scr.setup(width=800,height=600)
 scr.bgcolor("black")
 scr.title("Pong Game")
+scr.tracer(0)
 
+rightPaddle=Paddle(350,0)
+leftPaddle=Paddle(-350,0)
 
+scr.listen()
+scr.onkeypress(rightPaddle.goUp,"Up")
+scr.onkeypress(rightPaddle.goDown,"Down")
 
+scr.onkeypress(leftPaddle.goUp, "w")
+scr.onkeypress(leftPaddle.goDown, "s")
+
+isGameOn = True
+while isGameOn:
+	scr.update()
 
 
 
